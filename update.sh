85a1d4c1d2c7f9da5a7c02b9e6898a90c6a5cac9
Nish Shyam <nshyam668@gmail.com>
2020-05-30 19:03:58 +0100

    edited script

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export PATH=$PATH:/usr/local/bin/git-credential-osxkeychain
 /usr/local/bin/jupyter nbconvert --to notebook --execute --inplace report.ipynb
 git add .
 git commit -m "daily update jupyter"
